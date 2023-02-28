2b7f0f997e0baf95e0f8fdd131ed304cebeca2fa
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2023-02-27 19:07:45 -0500

    Wired in proxy and ping button

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+  const ping = async () => {
+    const response = await fetch("/ping");
+    const data = await response.text();
+    setMessage(data);
+  };
   return (
     <>
       <h1>Strickland Propane</h1>
       <h2>Coming Soon!</h2>
+      <button onClick={ping}>Ping The Server</button>
+      <p>Server responded: {message}</p>
     </>
   );
 }
