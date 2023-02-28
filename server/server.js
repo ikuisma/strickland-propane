b45f7e8de99f26c09551409ff35c38c0d96b6fa8
Ilkka Kuisma <ilkka.a.kuisma@gmail.com>
2023-02-27 19:00:35 -0500

    Added build script for root project

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Bootcamp!");
-});
+const PORT = process.env.PORT || 3001;
+const BUILD_PATH = path.resolve("../client/build");
+
+app.use(express.static(BUILD_PATH));
 
 app.get("/ping", (req, res) => {
   res.send("pong!");
